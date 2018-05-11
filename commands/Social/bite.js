27208abf1c7dc5de7465ed252431830fa180b0b7
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-11 17:50:35 +0200

    Update bite.js

--- commands/Social/bite.js
+++ commands/Social/bite.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 
   var bite = ["https://cdn.discordapp.com/attachments/437680262457851924/441681522915803136/photofunky.gif","https://data.photofunky.net/output/image/c/a/2/a/ca2abf/photofunky.gif","https://cdn.discordapp.com/attachments/437680262457851924/441680676987863050/telechargement.gif","https://cdn.discordapp.com/attachments/437680262457851924/441680676446535680/Chihuahua_mechant.gif","https://cdn.discordapp.com/attachments/437680262457851924/441673904436543498/49dc99422710d7294f3c350f4c58eb2916481c07_hq.gif", "https://cdn.discordapp.com/attachments/437680262457851924/441829639438991360/3252462046_1_9_zHieibKF.gif" ]
-
+let defineduser = message.mentions.users.first();
  var biting = bite[Math.floor(Math.random() * bite.length)] ;
  if (!defineduser){
        var biteEmbed = new Discord.RichEmbed()
