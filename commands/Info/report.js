b745382da495138e70bec54abf79136dfc98a985
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-11 18:13:08 +0200

    Update report.js

--- commands/Info/report.js
+++ commands/Info/report.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
 
+	let defineduser = message.mentions.users.first();
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Je ne trouve pas cette utilisateur").then(msg => {msg.delete(5000)});
       let rreason = args.join(" ").slice(31);
