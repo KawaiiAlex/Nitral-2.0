f80de42aa20c3fe8287fccd0d03a52bdb5fabbb9
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-11 17:53:25 +0200

    Update punch.js

--- commands/Social/punch.js
+++ commands/Social/punch.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 
   var punch = ["https://cdn.weeb.sh/images/BJg7wTbbM.gif", "https://cdn.weeb.sh/images/rJHLDT-Wz.gif","https://cdn.weeb.sh/images/HJfGPTWbf.gif", "https://78.media.tumblr.com/f0032ce15e4ded204b83b00c764a52ad/tumblr_olcckgOtiA1qzxv73o1_r4_500.gif","http://media.giphy.com/media/11zD6xIdX4UOfS/giphy.gif","https://media.tenor.co/images/7a582f32ef2ed527c0f113f81a696ae3/tenor.gif","https://media.tenor.co/images/7a582f32ef2ed527c0f113f81a696ae3/tenor.gif","http://www.videovortex.evan-roth.com/gifs2/Punch-Anime.gif","http://gph.is/15zjo9x"]
-
+let defineduser = message.mentions.users.first();
   var punching = punch[Math.floor(Math.random() * punch.length)] ;
   if (!defineduser){
   var punchEmbed = new Discord.RichEmbed()
