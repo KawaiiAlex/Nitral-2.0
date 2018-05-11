46dd9ea87979a2e9654fd8020f47c9baa9059929
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-11 17:50:47 +0200

    Update cry.js

--- commands/Social/cry.js
+++ commands/Social/cry.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
-
+let defineduser = message.mentions.users.first();
   var cry = ["https://cdn.weeb.sh/images/rknUmIXD-.gif", "https://cdn.weeb.sh/images/B1Jg1eqJM.gif", "https://cdn.weeb.sh/images/BkoBX8mwW.gif", "https://cdn.weeb.sh/images/HJIpry35M.gif", "https://cdn.weeb.sh/images/SJ08mUXwZ.gif","https://cdn.weeb.sh/images/Syzw78XPZ.gif" ,"https://cdn.weeb.sh/images/HymMXUQPW.gif" , "https://cdn.weeb.sh/images/ryap_aEC-.gif" ,"https://cdn.weeb.sh/images/SJ-11x5kz.gif" ,"https://cdn.weeb.sh/images/Bk_fmL7wZ.gif" ,"https://cdn.weeb.sh/images/rJ5IX8XPZ.gif" ]
 var crying = cry[Math.floor(Math.random() * cry.length)] ;
 
