c7c07435ff778791109ac1fab7e6419ebeb8f841
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-12 16:51:07 +0200

    Update channel.js

--- commands/Info/channel.js
+++ commands/Info/channel.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-exports.run = (client, message) => {
+exports.run = (client, message, args) => {
 
 let channel = message.mentions.channels.first();
   if (!channel) {
