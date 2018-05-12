046514970df822d38a53c8b2409d5c7e9bc29669
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-12 06:23:48 +0200

    Update rps.js

--- commands/Fun/rps.js
+++ commands/Fun/rps.js
@@ -1,36 +1,39 @@
 const Discord = require('discord.js')
 
+function rand(low, high) {
+    return Math.random() * (high + 1 - low) + low | 0;
+}
+
 exports.run = (client, message, args) => {
 
 
-  if (args[0]) {
-    // get user choice && user choice
-    let computer_choice = rand(0,2);
-    let user_choice = args[0] == "pierre " ? 1 : args[0] == "feuille " ? 2 : 0;
+  /*
+   * 0 -> scissors
+   * 1 -> rock
+   * 2 -> paper
+   */
+  run : (args, Client, msg) => {
+    if (args[0]) {
+      // get user choice && user choice
+      let computer_choice = rand(0,2);
+      let user_choice = args[0] == "rock" ? 1 : args[0] == "paper" ? 2 : 0;
 
-    // if their choices are same its a draw :D
-    if (computer_choice == user_choice) {
-      message.channel.send("Egalité!");
+      // if their choices are same its a draw :D
+      if (computer_choice == user_choice) {
+        msg.reply("Game Drawn!");
+      }
+      else if (computer_choice < user_choice || computer_choice == 0 && user_choice == 2) {
+        msg.reply("Computer Won!");
+      } else {
+        msg.reply("You Won!");
+      }
     }
-     if (computer_choice == 0 && user_choice == 2) {
-      message.channel.send (  " J'ai gagné!");
-    }
-  if (computer_choice == 2 && user_choice == 0) {
-      message.channel.send ("Tu as gagné!");
-    }
-    if (computer_choice == 1 && user_choice == 0) {
-      message.channel.send ("J'ai gagné !");
-    }
-  if (computer_choice == 0 && user_choice == 1) {
-      message.channel.send ("Tu as gagné !");
-    }
-    if (computer_choice == 1 && user_choice == 2) {
-      message.channel.send ("Tu as gagné !");
-    }
-    if (computer_choice == 2 && user_choice == 1) {
-      message.channel.send ("J'ai gagné !");
-
-    }
+  },
+  usage : () => {
+    return "+rps rock/paper/scissors";
+  },
+  description : () => {
+    return "Used for playing rock, paper, scissors against the bot.";
   }
 }
 
