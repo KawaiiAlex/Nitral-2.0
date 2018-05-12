74dcee60b286bac103db732b1923ba106de124e2
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-12 17:18:50 +0200

    Update userinfo.js

--- commands/Info/userinfo.js
+++ commands/Info/userinfo.js
@@ -32,6 +32,11 @@ exports.run = async (client, message, args) => {
       definedUser = mention || member.user
         definedUser2 = message.mentions.members.first() || message.guild.members.get(args[0]) || member
     }
+  const millisCreated = new Date().getTime() - definedUser.createdAt.getTime();
+    const daysCreated = millisCreated / 1000 / 60 / 60 / 24;
+
+    const millisJoined = new Date().getTime() - member.joinedAt.getTime();
+const daysJoined = millisJoined / 1000 / 60 / 60 / 24;
   
   let uEmbed = new Discord.RichEmbed()
   .setDescription("**Informations Utilisateurs **")
@@ -43,8 +48,8 @@ exports.run = async (client, message, args) => {
   .addField("**Bot**", `${definedUser.bot ? "Oui" : "Non"}`, true)
   .addField("**Statuts**",definedUser.presence.status, true)
   .addField("**Jeu**", `${definedUser.presence.game ? `${definedUser.presence.game.name}` : "Ne joue a rien"}`, true)
-  .addField("**Création du compte**", `${moment.utc(definedUser.createdAt).format("D/M/Y, HH:mm:ss")} (${ms(Date.now()- moment.utc(definedUser.createdAt), {long: true})})`)
-  .addField("**Date d'arrivée sur le serv**", `${moment.utc(definedUser.joinedAt).format("D/M/Y, HH:mm:ss")}`);
+  .addField("**Création du compte**", `${moment.utc(definedUser.createdAt).format("D/M/Y, HH:mm:ss")} ${daysCreated.toFixed(0)} jours`)
+  .addField("**Date d'arrivée sur le serv**", `${moment.utc(definedUser.joinedAt).format("D/M/Y, HH:mm:ss")} ${daysJoined.toFixed(0)} jours`);
 
   message.channel.send(uEmbed);
 }
