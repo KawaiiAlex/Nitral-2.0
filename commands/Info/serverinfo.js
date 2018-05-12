2d5c1957a833cefdff9e1871817beab189287f28
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-12 17:33:54 +0200

    Update serverinfo.js

--- commands/Info/serverinfo.js
+++ commands/Info/serverinfo.js
@@ -3,32 +3,9 @@ const moment = require ("moment")
 const ms = require("ms");
 exports.run = (client, message, args) => {
   
-  if(message.author.bot) return;
-  if(message.channel.type !== "text") return;
-  
   let members = [];
   let indexes = [];
-  
-  message.guild.members.forEach(function(member){
-    members.push(member.user.username);
-    indexes.push(member.id);
-  })
-  
-  let match = sm.findBestMatch(args.join(' '), members);
-  let username = match.bestMatch.target;
-  
-    let member = message.guild.members.get(indexes[members.indexOf(username)])
-    
-     let definedUser = "";
-     let definedUser2 = "";
-    if(!args[0]) {
-      definedUser = message.author
-      definedUser2 = message.member
-    } else {
-      let mention = message.mentions.users.first()
-      definedUser = mention || member.user
-        definedUser2 = message.mentions.members.first() || message.guild.members.get(args[0]) || member
-    }
+
   const millisCreated = new Date().getTime() - message.guild.createdAt.getTime();
     const daysCreated = millisCreated / 1000 / 60 / 60 / 24;
 
