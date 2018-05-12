45dc8b78d8b10bde0e0c0b3ae6832e1e80b18c13
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-12 10:35:35 +0200

    Update rps.js

--- commands/Fun/rps.js
+++ commands/Fun/rps.js
@@ -1,45 +1,72 @@
-const Discord = require('discord.js')
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+const { Command } = require('discord.js-commando');
+const choices = ['pierre', 'papier', 'ciseaux'];
 
 exports.run = (client, message, args) => {
 
-	if(!args[0]){
-		message.channel.send ("merci de choisir pierre, papier, ciseaux") 
-} else{
-  var msg1 = Array(3);
-		msg1[1] = "pierre :black_circle:";
-	    msg1[2] = "papier :page_facing_up:";
-		msg1[3] = "ciseaux :scissors:"
-        var x = getRandomInt(0, 9);
-		if (x < 6){
-         if (x < 3){
-			message.channel.send(msg1[1]);
+
+	if (message, { playerChoice }) {
+		const botChoice = choices[Math.floor(Math.random() * choices.length)];
+		const botWon = `J'ai gagné, j'ai choisi ${botChoice}!`;
+		const playerWon = `${message.member.displayName} à gagné la parti ! j'ai  choisie ${botChoice}!`;
+		const draw = `Égalité ! J'ai choisi ${botChoice}!`;
+
+		switch (playerChoice) {
+			case 'pierre':
+				switch (botChoice) {
+		 	  	case 'pierre':
+						message.say(draw);
+						break;
+					case 'papier':
+						message.say(botWon);
+						break;
+					case 'ciseaux':
+						message.say(playerWon);
+						break;
+				}
+				break;
+			case 'papier':
+				switch (botChoice) {
+					case 'pierre':
+						message.say(playerWon);
+						break;
+					case 'papier':
+						message.say(draw);
+						break;
+					case 'ciseaux':
+		  			message.say(botWon);
+						break;
+				}
+				break;
+			case 'ciseaux':
+				switch (botChoice) {
+					case 'pierre':
+						message.say(botWon);
+						break;
+					case 'papier':
+						message.say(playerWon);
+						break;
+					case 'ciseaux':
+						message.say(draw);
+						break;
+				}
+				break;
+			default:
+				return message.channel.send ('Merci de préciser votre choix : pierre, feuilles, ciseaux ');
+
 		}
-		else{
-               message.channel.sendMessage(msg1[3]);
-		}
-		}
-		else{ 
-			message.channel.sendMessage(msg1[2]);
-		}
-	}/*else if(err) {
-	message.channel.sendMessage("En cours de développement ");
-	}*/
-} 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['si'],
-  permLevel: 0
+
+	}
+
 };
 
+exports.conf = { 
+	enabled: true, 
+	guildOnly: false, 
+	aliases: [], 
+	permLevel: 0
+}; 
 exports.help = {
-  name: 'rps',
-  description: 'Fait un pierre feuille ciseau avec le bot',
-  usage: 'rps [pierre , papier , ciseau]'
-};
+	 name: 'rps', 
+	 description: 'Fait un pierre feuille ciseau avec le bot ', 
+	 usage: 'rps pierre, feuilles, ciseaux ' };
+
